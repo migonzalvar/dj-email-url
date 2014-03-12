28334404c5892f92c659ee542a138abbef2eb11b
Miguel Gonzalez <migonzalvar@gmail.com>
2014-03-12 22:51:59 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='dj-email-url',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/migonzalvar/dj-email-url',
     license='BSD',
     author='Miguel Gonzalez',
