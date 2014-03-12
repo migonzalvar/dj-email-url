4fbc7427e0329c560ee56105192809e482022930
Miguel Gonzalez <migonzalvar@gmail.com>
2014-03-12 22:50:50 +0100

    Use long description from README.rst

--- setup.py
+++ setup.py
@@ -1,61 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-dj-email-url
-~~~~~~~~~~~~
-
-This utiliy is based on dj-database-url by Kenneth Reitz.
-
-It allows to utilize the
-`12factor <http://www.12factor.net/backing-services>`_ inspired
-environments variable to configure the email backend in a Django application.
-
-Usage
------
-
-Configure your email configuration values in ``settings.py`` from
-``EMAIL_URL``::
-
-    email_config = dj_email_url.config()
-
-Parse an arbitrary email URL::
-
-    email_config = dj_email_url.parse('smtp://...')
-
-
-After this, it is necessary to assign values to settings::
-
-    EMAIL_FILE_PATH = email_config['EMAIL_FILE_PATH']
-    EMAIL_HOST_USER = email_config['EMAIL_HOST_USER']
-    EMAIL_HOST_PASSWORD = email_config['EMAIL_HOST_PASSWORD']
-    EMAIL_HOST = email_config['EMAIL_HOST']
-    EMAIL_PORT = email_config['EMAIL_PORT']
-    EMAIL_BACKEND = email_config['EMAIL_BACKEND']
-
-
-Supported backends
-------------------
-
-Currently, it supports:
-
-- SMTP backend (smtp or smtps),
-
-- console backend (console),
-
-- file backend (file),
-
-- in-memory backend (memory),
-
-- and dummy backend (dummy).
-
-
-The scheme ``smtps`` indicates to use TLS connections, that is to set
-``EMAIL_USE_TLS`` to ``True``.
-
-The file backend is the only one which needs a path. The url path is store
-in ``EMAIL_FILE_PATH`` key.
-"""
 
 from setuptools import setup
+
+
+with open('README.rst') as f:
+    long_description = f.read()
 
 setup(
     name='dj-email-url',
@@ -65,7 +14,7 @@ setup(
     author='Miguel Gonzalez',
     author_email='migonzalvar@gmail.com',
     description='Use an URL to configure email backend settings in your Django Application.',
-    long_description=__doc__,
+    long_description=long_description,
     py_modules=['dj_email_url'],
     zip_safe=False,
     include_package_data=True,
