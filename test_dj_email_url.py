39123c0506adcaf0a6f18bb9dd83fc8e63e5962f
Miguel Gonzalez <migonzalvar@gmail.com>
2015-03-04 22:55:40 +0100

    PEP8

--- test_dj_email_url.py
+++ test_dj_email_url.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import unittest
@@ -17,29 +17,29 @@ class EmailTestSuite(unittest.TestCase):
         url = dj_email_url.parse(url)
 
         assert url['EMAIL_BACKEND'] == \
-                'django.core.mail.backends.smtp.EmailBackend'
+            'django.core.mail.backends.smtp.EmailBackend'
         assert url['EMAIL_HOST'] == 'smtp.example.com'
         assert url['EMAIL_HOST_PASSWORD'] == 'password'
         assert url['EMAIL_HOST_USER'] == 'user@domain.com'
         assert url['EMAIL_PORT'] == 587
-        assert url['EMAIL_USE_TLS'] == True
+        assert url['EMAIL_USE_TLS'] is True
 
     def test_email_url(self):
         a = dj_email_url.config()
         assert not a
 
         os.environ['EMAIL_URL'] = \
-                'smtps://user@domain.com:password@smtp.example.com:587'
+            'smtps://user@domain.com:password@smtp.example.com:587'
 
         url = dj_email_url.config()
 
         assert url['EMAIL_BACKEND'] == \
-                'django.core.mail.backends.smtp.EmailBackend'
+            'django.core.mail.backends.smtp.EmailBackend'
         assert url['EMAIL_HOST'] == 'smtp.example.com'
         assert url['EMAIL_HOST_PASSWORD'] == 'password'
         assert url['EMAIL_HOST_USER'] == 'user@domain.com'
         assert url['EMAIL_PORT'] == 587
-        assert url['EMAIL_USE_TLS'] == True
+        assert url['EMAIL_USE_TLS'] is True
 
 
 if __name__ == '__main__':
